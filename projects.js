5efb2207a1d2fa71cfa1f6efc51cb9bd77938303
Surbhi Sharma <sharma.surbhi13@gmail.com>
2017-08-22 20:22:47 -0400

    initial commit-JS

---
+++ projects.js
@@ -0,0 +1,15 @@
+function sum(num) {
+  return num + 2;
+}
+
+var newArray = [1, 2, 3, 4, 5];
+function addArray(arr) {
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return sum;
+}
+
+addArray(newArray);
